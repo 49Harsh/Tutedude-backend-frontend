39d54fe03d7b0f93b5f1a2067b28c806a301628a
Harsh-Portfolio <49harshyadav@gmail.com>
2024-09-15 21:32:27 +0530

    adding backend deploy url in axios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://tutedude-backend-frontend-1.onrender.com',
 });
 
 export default instance;
